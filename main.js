0e73681894d4890cb59bed66249fc03fa5cf9811
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-05-26 18:56:29 +0900

    result 로직 추가

--- main.js
+++ main.js
@@ -4,14 +4,119 @@ const result = document.getElementById("result");
 const q = document.querySelector(".questionContainer");
 const a = document.querySelector(".anwerContainer");
 const status = document.querySelector(".statusBar");
+const resultName = document.querySelector(".resultName");
+const resultImg = document.querySelector(".resultImg");
+const resultDesc = document.querySelector(".resultDesc");
 
 let endPoint = 5;
 let select = 0;
 
 const goResult = () => {
-  console.log(select);
   qna.classList.add("hide");
   result.classList.remove("hide");
+  let num = select.toString();
+  console.log(num);
+  if (select > 2000) {
+    //소형
+    if (select > 2200) {
+      // 무늬
+      if (select < 2260) {
+        //음지
+        if (num.indexOf("7") === 3) {
+          //한달
+          console.log("소형 무늬 음지 한달");
+        } else if (num.indexOf("8") === 3) {
+          //2week
+        } else if (num.indexOf("9") === 3) {
+          //1week
+        }
+      } else if (select >= 2260) {
+        // 양지
+        if (num.indexOf("7") === 3) {
+          //한달
+          console.log("소형 무늬 양지 한달");
+        } else if (num.indexOf("8") === 3) {
+          //2week
+        } else if (num.indexOf("9") === 3) {
+          //1week
+          console.log("소형 무늬 양지 일주");
+        }
+      }
+    } else if (select < 2200) {
+      //무난
+      if (select < 2160) {
+        //음지
+        if (num.indexOf("7") === 3) {
+          //한달
+          console.log("소형 무난 음지 한달");
+        } else if (num.indexOf("8") === 3) {
+          //2week
+        } else if (num.indexOf("9") === 3) {
+          //1week
+        }
+      } else if (select >= 2160) {
+        // 양지
+        if (num.indexOf("7") === 3) {
+          //한달
+          console.log("소형 무난 양지 한달");
+        } else if (num.indexOf("8") === 3) {
+          //2week
+        } else if (num.indexOf("9") === 3) {
+          //1week
+        }
+      }
+    }
+  } else {
+    //대형
+    if (select > 1200) {
+      // 무늬
+      if (select < 1260) {
+        //음지
+        if (num.indexOf("7") === 3) {
+          //한달
+          console.log("대형 무늬 음지 한달");
+        } else if (num.indexOf("8") === 3) {
+          //2week
+        } else if (num.indexOf("9") === 3) {
+          //1week
+        }
+      } else if (select >= 1260) {
+        // 양지
+        if (num.indexOf("7") === 3) {
+          //한달
+          console.log("대형 무늬 양지 한달");
+        } else if (num.indexOf("8") === 3) {
+          //2week
+        } else if (num.indexOf("9") === 3) {
+          //1week
+          console.log("대형 무늬 양지 일주");
+        }
+      }
+    } else if (select < 1200) {
+      //무난
+      if (select < 1160) {
+        //음지
+        if (num.indexOf("7") === 3) {
+          //한달
+          console.log("대형 무난 음지 한달");
+        } else if (num.indexOf("8") === 3) {
+          //2week
+        } else if (num.indexOf("9") === 3) {
+          //1week
+        }
+      } else if (select >= 1160) {
+        // 양지
+        if (num.indexOf("7") === 3) {
+          //한달
+          console.log("대형 무난 양지 한달");
+        } else if (num.indexOf("8") === 3) {
+          //2week
+        } else if (num.indexOf("9") === 3) {
+          //1week
+        }
+      }
+    }
+  }
 };
 
 const addAnswer = (answerText, count, idx) => {
