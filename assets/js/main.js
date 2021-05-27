90661c34c9c4b93b1acd6d9caa160cc3b0f19c9c
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-05-27 15:47:09 +0900

    add resultList,imgs and favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,25 +26,40 @@ const goResult = () => {
           //한달
           img.src = resultList[0].img;
           resultName.innerText = resultList[0].name;
-          resultDesc.innerText = resultList[0].desc;
+          resultDesc.innerHTML = resultList[0].desc;
           console.log("소형 무늬 음지 한달");
         } else if (num.indexOf("8") === 3) {
           //2week
+          img.src = resultList[1].img;
+          resultName.innerText = resultList[1].name;
+          resultDesc.innerHTML = resultList[1].desc;
           console.log("소형 무늬 음지 2주");
         } else if (num.indexOf("9") === 3) {
           //1week
+          img.src = resultList[2].img;
+          resultName.innerText = resultList[2].name;
+          resultDesc.innerHTML = resultList[2].desc;
           console.log("소형 무늬 음지 1주");
         }
       } else if (select >= 2260) {
         // 양지
         if (num.indexOf("7") === 3) {
           //한달
+          img.src = resultList[3].img;
+          resultName.innerText = resultList[3].name;
+          resultDesc.innerHTML = resultList[3].desc;
           console.log("소형 무늬 양지 한달");
         } else if (num.indexOf("8") === 3) {
           //2week
+          img.src = resultList[4].img;
+          resultName.innerText = resultList[4].name;
+          resultDesc.innerHTML = resultList[4].desc;
           console.log("소형 무늬 양지 2주");
         } else if (num.indexOf("9") === 3) {
           //1week
+          img.src = resultList[5].img;
+          resultName.innerText = resultList[5].name;
+          resultDesc.innerHTML = resultList[5].desc;
           console.log("소형 무늬 양지 일주");
         }
       }
@@ -54,24 +69,42 @@ const goResult = () => {
         //음지
         if (num.indexOf("7") === 3) {
           //한달
+          img.src = resultList[6].img;
+          resultName.innerText = resultList[6].name;
+          resultDesc.innerHTML = resultList[6].desc;
           console.log("소형 무난 음지 한달");
         } else if (num.indexOf("8") === 3) {
           //2week
+          img.src = resultList[7].img;
+          resultName.innerText = resultList[7].name;
+          resultDesc.innerHTML = resultList[7].desc;
           console.log("소형 무난 음지 2주");
         } else if (num.indexOf("9") === 3) {
           //1week
+          img.src = resultList[8].img;
+          resultName.innerText = resultList[8].name;
+          resultDesc.innerHTML = resultList[8].desc;
           console.log("소형 무난 음지 1주");
         }
       } else if (select >= 2160) {
         // 양지
         if (num.indexOf("7") === 3) {
           //한달
+          img.src = resultList[9].img;
+          resultName.innerText = resultList[9].name;
+          resultDesc.innerHTML = resultList[9].desc;
           console.log("소형 무난 양지 한달");
         } else if (num.indexOf("8") === 3) {
           //2week
+          img.src = resultList[10].img;
+          resultName.innerText = resultList[10].name;
+          resultDesc.innerHTML = resultList[10].desc;
           console.log("소형 무난 양지 2주");
         } else if (num.indexOf("9") === 3) {
           //1week
+          img.src = resultList[11].img;
+          resultName.innerText = resultList[11].name;
+          resultDesc.innerHTML = resultList[11].desc;
           console.log("소형 무난 양지 1주");
         }
       }
@@ -84,24 +117,42 @@ const goResult = () => {
         //음지
         if (num.indexOf("7") === 3) {
           //한달
+          img.src = resultList[12].img;
+          resultName.innerText = resultList[12].name;
+          resultDesc.innerHTML = resultList[12].desc;
           console.log("대형 무늬 음지 한달");
         } else if (num.indexOf("8") === 3) {
           //2week
+          img.src = resultList[13].img;
+          resultName.innerText = resultList[13].name;
+          resultDesc.innerHTML = resultList[13].desc;
           console.log("대형 무늬 음지 2주");
         } else if (num.indexOf("9") === 3) {
           //1week
+          img.src = resultList[14].img;
+          resultName.innerText = resultList[14].name;
+          resultDesc.innerHTML = resultList[14].desc;
           console.log("대형 무늬 음지 1주");
         }
       } else if (select >= 1260) {
         // 양지
         if (num.indexOf("7") === 3) {
           //한달
+          img.src = resultList[15].img;
+          resultName.innerText = resultList[15].name;
+          resultDesc.innerHTML = resultList[15].desc;
           console.log("대형 무늬 양지 한달");
         } else if (num.indexOf("8") === 3) {
           //2week
+          img.src = resultList[16].img;
+          resultName.innerText = resultList[16].name;
+          resultDesc.innerHTML = resultList[16].desc;
           console.log("대형 무늬 양지 2주");
         } else if (num.indexOf("9") === 3) {
           //1week
+          img.src = resultList[17].img;
+          resultName.innerText = resultList[17].name;
+          resultDesc.innerHTML = resultList[17].desc;
           console.log("대형 무늬 양지 일주");
         }
       }
@@ -111,24 +162,42 @@ const goResult = () => {
         //음지
         if (num.indexOf("7") === 3) {
           //한달
+          img.src = resultList[18].img;
+          resultName.innerText = resultList[18].name;
+          resultDesc.innerHTML = resultList[18].desc;
           console.log("대형 무난 음지 한달");
         } else if (num.indexOf("8") === 3) {
           //2week
+          img.src = resultList[19].img;
+          resultName.innerText = resultList[19].name;
+          resultDesc.innerHTML = resultList[19].desc;
           console.log("대형 무난 음지 2주");
         } else if (num.indexOf("9") === 3) {
           //1week
+          img.src = resultList[20].img;
+          resultName.innerText = resultList[20].name;
+          resultDesc.innerHTML = resultList[20].desc;
           console.log("대형 무난 음지 1주");
         }
       } else if (select >= 1160) {
         // 양지
         if (num.indexOf("7") === 3) {
           //한달
+          img.src = resultList[21].img;
+          resultName.innerText = resultList[21].name;
+          resultDesc.innerHTML = resultList[21].desc;
           console.log("대형 무난 양지 한달");
         } else if (num.indexOf("8") === 3) {
           //2week
+          img.src = resultList[22].img;
+          resultName.innerText = resultList[22].name;
+          resultDesc.innerHTML = resultList[22].desc;
           console.log("대형 무난 양지 2주");
         } else if (num.indexOf("9") === 3) {
           //1week
+          img.src = resultList[23].img;
+          resultName.innerText = resultList[23].name;
+          resultDesc.innerHTML = resultList[23].desc;
           console.log("대형 무난 양지 1주");
         }
       }
