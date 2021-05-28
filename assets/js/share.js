e37afcc1804532847c9020b1e0d46f3914d7014f
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-05-28 21:26:46 +0900

    share btn modify

--- assets/js/share.js
+++ assets/js/share.js
@@ -1,35 +1,28 @@
-const kakaoShare = () => {
-  Kakao.Link.createDefaultButton({
-    container: "#CONTAINER_ID",
+function kakaoShare() {
+  Kakao.Link.sendDefault({
     objectType: "feed",
     content: {
-      title: "디저트 사진",
-      description: "아메리카노, 빵, 케익",
-      imageUrl:
-        "http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
+      title: "나만의 반려식물 찾기 테스트 🌱",
+      description: "테스트를 통해 당신에게 딱 맞는 식물을 추천해 드릴게요!",
+      imageUrl: "../img/find_my_plant-01.png",
       link: {
-        mobileWebUrl: "https://developers.kakao.com",
-        androidExecParams: "test",
+        mobileWebUrl: "https://findmyplant.netlify.app/",
+        webUrl: "https://findmyplant.netlify.app/",
       },
     },
     social: {
-      likeCount: 10,
-      commentCount: 20,
-      sharedCount: 30,
+      likeCount: 286,
+      commentCount: 45,
+      sharedCount: 845,
     },
     buttons: [
       {
-        title: "웹으로 이동",
+        title: "웹으로 보기",
         link: {
-          mobileWebUrl: "https://developers.kakao.com",
-        },
-      },
-      {
-        title: "앱으로 이동",
-        link: {
-          mobileWebUrl: "https://developers.kakao.com",
+          mobileWebUrl: "https://findmyplant.netlify.app/",
+          webUrl: "https://findmyplant.netlify.app/",
         },
       },
     ],
   });
-};
+}
